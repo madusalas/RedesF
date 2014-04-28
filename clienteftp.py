9af3f758469823d5149c9dc1fbfb4b4371fa212d
madusalas <maru.uno@gmail.com>
2014-04-28 11:35:21 -0500

    Update clienteftp.py

--- clienteftp.py
+++ clienteftp.py
@@ -1,219 +1,63 @@
-iimport socket
-import time
-import getpass
+import asyncore
+import socket
+import SSL
 
-user = 'userftp'
-PASSW = 'r3d3sf1s1c@s'
+# Client Section
 
-HOST = '192.100.230.21'
-PORT = 21
-#espacio de memoria reservada para el archivo 2048
-def tiempo(s):
-	timeout=1
-    s.setblocking(0) #se declara el socket para que no se cierre
-    Tdatos=[] #se declara una lista
-    inicio=time.time() #inicia timer sin parametros
-    while True: #deja abierto el socket con el bucle
-        if Tdatos and time.time()-inicio>timeout: #checa si hay datos en la lista o si el incio - el tiempo actual son mayor que timeout
-            break
-        elif time.time()-inicio>timeout*2:
-            break
-        try:
-            datos=s.recv(2048) 
-            if datos:
-                 Tdatos.append(datos)
-                 inicio=time.time()
-            else:
-                time.sleep(0.2)
-        except:
-            pass
-    return ''.join(Tdatos)
+class Cliente(asyncore.dispatcher):
 
-def recv_archivo(s,archivo):
-	timeout=1
-    s.setblocking(0)
-    Rdatos=False #datos recibidos
-    inicio=time.time()
-    archivo = open(archivo, 'archivo')
-    while True:
-        if Rdatos and time.time()-inicio>timeout:
-            break
-        try:
-            datos=s.recv(2048)
-            if datos:
-                Rdatos = True
-                archivo.write(datos)
-              
-                inicio=time.time()
-            else:
-                time.sleep(1)
-        except:
-            pass
-    archivo.close()
+    def __init__(self, host, port):
+        asyncore.dispatcher.__init__(self)
+        self.create_socket(socket.AF_INET, socket.SOCK_STREAM) #conexion C/S
+        self.connect((host, port))
+    connectionPhase = 1
+	
+    def handle_read(self): #manejador de respuestas
+        print "connectionPhase =", self.connectionPhase
+    if self.connectionPhase < 3: # authentication phase
+            data = self.recv(1024) #espero recibir los datos con un buffer de 1024
+            print 'Received:', data
+         # connectionPhase = 3 when this IF loop is done
+        elif self.connectionPhase == 3: # receiving data for User
+            data = self.recv(1024) #recv(buffer_size)
+            print 'Received data - forward to User:', data
 
-def peticion(s,mej):
-	peticion = mej
-    s.send(peticion)
-    respuesta = tiempo(s)
-    return respuesta
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((host, port))
-    return s
+# Connects to User
+class RelayConnection(asyncore.dispatcher):
+    def __init__(self, client, sock):
+        asyncore.dispatcher.__init__(self)
+        self.client = client
+        print "connecting to %s..." % str(sock)
 
-def respuesta(response, FTPcomando):
-    if response == '':
-        return False
-    Val = True
-    for a in response.splitlines():
-        if int(response[0:3]) not in responsehash[FTPcomando]:
-            Val = False
-             break
-    return Val
+    def handle_connect(self):
+        print "connected."
+        # Allow reading once the connection on the other side is open.
+        self.client.is_readable = True
 
-def ls(s):
-    socketArchivo = socket1(s)
-    if socketArchivo == None:
-        print 'No se pudo'
-        return False
+    def handle_read(self):
+        self.client.send(self.recv(1024))
 
-    if not respuesta(peticion(s, 'LIST'), 'LIST'):
-        print 'No se pudo'
-        return False
+class RelayClient(asyncore.dispatcher):
+    def __init__(self, server, client, sock):
+        asyncore.dispatcher.__init__(self, client)
+        self.is_readable = False
+        self.relay = RelayConnection(self, sock)
 
-    print tiempo(socketArchivo)
+    def handle_read(self):
+        self.relay.send(self.recv(1024))
 
-    socketArchivo.close()
+    def handle_close(self):
+        print "Closing..."
+        # If the client disconnects, close the relay connection.
+        self.relay.close()
+        self.close()
 
-def login(s, user, passw):
-    if not respuesta(peticion(s, 'USER ' + user), 'USER'):
-        print 'No existe'
-        return False
-    if not respuesta(peticion(s, 'PASS ' + passw), 'PASS'):
-        print 'Contraseña erronea'
-        return False
-    print 'Conexion exitosa!!'
-    return True
+    def readable(self):
+        return self.is_readable
 
-def logout(s):
-    ret = True
-    if not respuesta(peticion(s, 'QUIT'), 'QUIT'):
-        print "Falló"
-        ret = False
-    s.close()
-    print "Desconectado"
-    return ret
+if __name__ == "__main__":
+    # First connection 
+    connectionPhase = 1
+    c = Cliente('192.100.230.21', 21) 
 
-def cd(s, path):
-    if not respuesta(peticion(s, 'CWD ' + path), 'CWD'):
-        print "No se puede ir al siguiente directorio " + path
-        return False
-    print 'Ok'
-    return True
-    
-def pwd(s):
-    response = peticion(s, 'PWD')
-    if not respuesta(response, 'PWD'):
-        print 'Falló'
-        return False
-    print response[4:]
-    return True
-
-def mkd(s, dir):
-    if not respuesta(peticion(s, 'MKD ' + dir), 'MKD'):
-        print "No se puede crear el directorio"
-        return False
-    print 'Ok'
-    return True
-
-def rmd(s, dir):
-    if not respuesta(peticion(s, 'RMD ' + dir), 'RMD'):
-        print "No se puede mover el directorio"
-        return False
-    print 'Ok'
- return True
-
-def upload(s, archivo):
-    file_stream = socket1(s)
-    if file_stream == None:
-        print 'Falló'
-        return False
-
-    if not respuesta(peticion(s, 'STOR ' + archivo), 'STOR'):
-        print"No se puede subir el archivo"
-        return False
-
-    buffer = "hola"
-    archivo = open(archivo, 'rb')
-    while True:
-        buffer = archivo.read(2048)
-        if buffer == "":
-            break
-        file_stream.send(buffer)
-    archivo.close()
-    file_stream.close()
- print tiempo(s)
-    print 'Ok'
-    return True
-
-def download(s, archivo):
-    file_stream = socket1(s)
-    if file_stream == None:
-        print 'Falló'
-        return False
-    if not respuesta(peticion(s, 'RETR ' + archivo), 'RETR'):
-        print "No se puede descargar el achivo"
-        return False
-    recv_archivo(file_stream, archivo+"_")
-    file_stream.close()
-    print tiempo(s)
-    print "Ok"
-    return True
-
-def rm(s, archivo):
-	if not respuesta(peticion(s, 'DELE ' + archivo), 'DELE'):
-        print "No se puede mover el archivo"
-        return False
-    print "Ok"
-    return True
-
-def connect(host, port):
-    clienteFTP = socket1(HOST, PORT)
-    if not respuesta(tiempo(clienteFTP), 'CONN'):
-        print "No se puede conectar al " + HOST + ':' + str(PORT)
-        clienteFTP = None
-    else:
-        print 'Conectado al ' + HOST + ':' + str(PORT)
-    return clienteFTP
-
-def main():
-    
-	while True:
-        parametro = str(raw_input()).split(' ')
-        FTPcomando = parametro[0]
-
-        if FTPcomando == "Conectado":
-            if parametro == 3:
-                host = parametro[1]
-                port = int(parametro[2])
-                clienteFTP = connect(host, port)
-            else:
-                clienteFTP = connect(HOST, PORT)
-            continue
-
-        if FTPcomando == 'login':
-            user = str(raw_input('Usuario: '))
-            passw = getpass.getpass()
-            if user == '':
-				login(clienteFTP, user, PASSW)
-            else:
-                login(clienteFTP, user, passw)
-			elif FTPcomando == 'pwd': pwd(clienteFTP)
-			elif FTPcomando == 'cd': cd(clienteFTP, parametro[1])
-			elif FTPcomando == 'ls': ls(clienteFTP)
-			elif FTPcomando == 'mkdir': mkd(clienteFTP, parametro[1])
-			elif FTPcomando == 'rm': rm(clienteFTP, parametro[1])
-			elif FTPcomando == 'rmd': rmd(clienteFTP, parametro[1])
-			elif FTPcomando == 'upload': upload(clienteFTP, parametro[1])
-			elif FTPcomando == 'download': download(clienteFTP, parametro[1])
-			elif FTPcomando == 'logout': logout(clienteFTP)
-			else: print "No existe esa operacion"
+    asyncore.loop()
